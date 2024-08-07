a91cc8aebda6b406516c2c9ffbbb963826e2b183
bskkim1999 <dbjewelrydb@gmail.com>
2024-08-07 16:23:29 +0900

    hello

--- 2024_08_07/numpy_ex_04.py
+++ 2024_08_07/numpy_ex_04.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-A = np.array([1, 2, 3, 4])
-B = np.array([5, 6, 7, 8])
-result = A + B		# 넘파이 배열에 + 연산이 적용된다. 
-print(result)
+arr1 = [1,2,3,4]
+arr2 = [5,6,7,8]
+
+A = np.array([1,2,3,4])
+B = np.array([5,6,7,8])
+
+result1 = arr1+arr2		# 넘파이 배열에 + 연산이 적용된다. 
+result2 = A+B
+
+print(result1)
+print(result2)
